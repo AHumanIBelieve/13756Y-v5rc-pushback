0d1ae519a6367c8a5ad38c0f0c4ea56eeb01884e
AHumanIBelieve <chaityajain@outlook.com>
2026-01-03 08:59:53 +0000

    Update main.py

--- pushback-comp/src/main.py
+++ pushback-comp/src/main.py
@@ -1,28 +1,21 @@
-# ---------------------------------------------------------------------------- #
-#                                                                              #
-# 	Module:       main.py                                                      #
-# 	Author:       Chaitya Jain                                                 #
-# 	Created:      9/22/2025, 4:13:08 PM                                        #
-# 	Description:  V5 project                                                   #
-#                                                                              #
-# ---------------------------------------------------------------------------- #
-
 # Library imports
 from vex import *
 
 brain = Brain()
 controller = Controller()
 # drivebase
-right_drive_back = Motor(Ports.PORT10)
-right_drive_front = Motor(Ports.PORT9, True)
-left_drive_back = Motor(Ports.PORT1)
-left_drive_front = Motor(Ports.PORT20, True)
+right_drive_back = Motor(Ports.PORT1)
+#right_drive_middle = Motor(Ports.PORT2)
+right_drive_front = Motor(Ports.PORT3, True)
+left_drive_back = Motor(Ports.PORT8)
+#left_drive_middle = Motor(Ports.PORT9)
+left_drive_front = Motor(Ports.PORT10, True)
 left_drive_smart = MotorGroup(left_drive_back, left_drive_front)
 right_drive_smart = MotorGroup(right_drive_back, right_drive_front)
 drivetrain = MotorGroup(left_drive_smart, right_drive_smart)
 # intake/lift
-#intake_motor = Motor(Ports.PORT18)
-extractor = DigitalOut(brain.three_wire_port.a)
+intake_motor = Motor(Ports.PORT18)
+#extractor = DigitalOut(brain.three_wire_port.a)
 
 # DRIVE CODE
 def drive():
@@ -46,50 +39,33 @@ def getDriveInput():
             i = i *-1
     return vals
 
-# # INTAKE CODE
-# intake_forward = False
-# intake_reverse = False
-# def intake():
-#     global intake_forward, intake_reverse, intake_motor
-#     while(True):
-#         if(controller.buttonL2.pressing()): #checks if button is pressed
-#             intake_motor.spin(FORWARD, 100, PERCENT)
-#         elif(controller.buttonR2.pressing()):
-#             intake_motor.spin(REVERSE, 100, PERCENT)
-#         else:
-#             intake_motor.spin(FORWARD, 0, PERCENT)
-#         sleep(10, MSEC)
-
-# LIFT CODE
-#lift_forward = False
-#left_reverse = False
-#banana = Motor(Ports.PORT5, True)
-#smartbanana = (banana)
-# def lift():
-#     global lift_forward, lift_reverse, lift_motor
-#     while(True):
-#         if(controller.buttonL1.pressing()): #checks if button is pressed
-#             banana.spin(FORWARD, 100, PERCENT)
-#         elif(controller.buttonR1.pressing()):
-#             banana.spin(REVERSE, 100, PERCENT)
-#         else:
-#             banana.spin(FORWARD, 0, PERCENT)
-#         sleep(10, MSEC)
-
-def extract():
-    extractor.set(False)
-    while True:
-        if(controller.buttonA.pressing()):
-            extractor.set(True)
-        elif(controller.buttonB.pressing()):
-            extractor.set(False)
+# def extract():
+#     extractor.set(False)
+#     while True:
+#         if(controller.buttonA.pressing()):
+#             extractor.set(True)
+#         elif(controller.buttonB.pressing()):
+#             extractor.set(False)
 
 def autonomous():
     controller.screen.print("brrrrrrrrr")
+# # INTAKE CODE
+intake_forward = False
+intake_reverse = False
+def intake():
+    global intake_forward, intake_reverse, intake_motor
+    while(True):
+        if(controller.buttonL2.pressing()): #checks if button is pressed
+            intake_motor.spin(FORWARD, 100, PERCENT)
+        elif(controller.buttonR2.pressing()):
+            intake_motor.spin(REVERSE, 100, PERCENT)
+        else:
+            intake_motor.spin(FORWARD, 0, PERCENT)
+        sleep(10, MSEC)
 
 # initialisation function for driver control. while loops will be within the functions.
 def usercontrol():
     Thread(drive)
-    Thread(extract)
+    Thread(intake)
  
 comp = Competition(usercontrol, autonomous)
